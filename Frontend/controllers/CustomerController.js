aeeaf21b26ea9b6a9ae390d7a141d92fdb10b612
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2023-12-14 14:38:46 +0530

    add ajax

--- Frontend/controllers/CustomerController.js
+++ Frontend/controllers/CustomerController.js
@@ -1,4 +1,17 @@
 let baseUrl = "http://localhost:8080/app/";
-function loadAllCustomers(){
 
+function loadAllCustomers() {
+    $.ajax({
+        url: baseUrl + "customer",
+        type: "get",
+        dataType: "json",
+        success: function (res) {
+
+        },
+        error:function (err) {
+
+        }
+    });
 }
+
+loadAllCustomers();
