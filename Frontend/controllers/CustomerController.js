9d2d629fd5f4417ebd5050a494e73097e1ba62cb
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2023-12-14 14:47:40 +0530

    add itarable content

--- Frontend/controllers/CustomerController.js
+++ Frontend/controllers/CustomerController.js
@@ -6,10 +6,24 @@ function loadAllCustomers() {
         type: "get",
         dataType: "json",
         success: function (res) {
-
+            $('#body').empty();
+            for (let customer in res.data) {
+                $('#body').append(`<tr>
+                    <td>${customer.id}</td>
+                    <td>${customer.name}</td>
+                    <td>${customer.address}</td>
+                    <td>${customer.salary}</td>
+                    <td><button type="button" class="btn btn-primary btn-sm me-2" data-bs-toggle="modal"
+                                        data-bs-target="#exampleModal2">
+                                    Edit
+                                </button>
+                                <button class="btn btn-danger me-3 btn-sm delete">Delete</button></td>
+                </tr>`);
+            }
         },
         error:function (err) {
-
+            let parse = JSON.parse(err.responseText);
+            alert(parse.message);
         }
     });
 }
