e3b3bf2ffd5a0cba2f758538d21069d152c2f287
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2023-12-14 15:00:38 +0530

    add get all function

--- Backend/src/main/java/lk/ijse/crud/CustomerServlet.java
+++ Backend/src/main/java/lk/ijse/crud/CustomerServlet.java
@@ -1,11 +1,15 @@
 package lk.ijse.crud;
 
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.*;
 
 /**
  * @author : savindaJ
@@ -16,6 +20,48 @@ import java.io.IOException;
 public class CustomerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("work");
+        System.out.println("come !");
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+        JsonArrayBuilder jsonArr = Json.createArrayBuilder();
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("register driver !");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade", "root", "80221474");
+            System.out.println("connection :"+connection);
+            PreparedStatement pstm = connection.prepareStatement("SELECT*from customer");
+            ResultSet set = pstm.executeQuery();
+            while (set.next()){
+                JsonObjectBuilder customer = Json.createObjectBuilder();
+                customer.add("id",set.getString(1));
+                customer.add("name",set.getString(2));
+                customer.add("address",set.getString(3));
+                customer.add("salary",set.getString(4));
+
+                jsonArr.add(customer.build());
+            }
+
+            JsonObjectBuilder obj = Json.createObjectBuilder();
+
+            obj.add("state", "OK");
+            obj.add("message", "Successfully Loaded..!");
+            obj.add("data", jsonArr);
+            resp.setStatus(200);
+
+            resp.getWriter().print(obj.build());
+
+            connection.close();
+
+        } catch (ClassNotFoundException | SQLException e) {
+            JsonObjectBuilder obj = Json.createObjectBuilder();
+
+            obj.add("state", "Error");
+            obj.add("message", e.getLocalizedMessage());
+            obj.add("data","");
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+
+            resp.getWriter().print(obj.build());
+            e.printStackTrace();
+        }
     }
 }
--- Frontend/controllers/CustomerController.js
+++ Frontend/controllers/CustomerController.js
@@ -23,23 +23,26 @@ function bindEvent() {
 
 function loadAllCustomers() {
     $.ajax({
-        url: baseUrl + "customer",
+        url: baseUrl+"customer",
         type: "get",
-        dataType: "json",
-        success: function (res) {
-            $('#body').empty();
-            for (let customer in res.data) {
-                $('#body').append(`<tr>
-                    <td>${customer.id}</td>
-                    <td>${customer.name}</td>
-                    <td>${customer.address}</td>
-                    <td>${customer.salary}</td>
-                    <td><button type="button" class="btn btn-primary btn-sm me-2" data-bs-toggle="modal"
+        dataType:"json",
+        success:function (resp) {
+            $(`#body`).empty();
+
+            for (const customer of resp.data) {
+
+                $(`#body`).append(`<tr>
+                                <td>${customer.id}</td>
+                                <td>${customer.name}</td>
+                                <td>${customer.address}</td>
+                                <td>${customer.salary}</td>
+                                <td><button type="button" class="btn btn-primary btn-sm me-2" data-bs-toggle="modal"
                                         data-bs-target="#exampleModal2">
                                     Edit
                                 </button>
                                 <button class="btn btn-danger me-3 btn-sm delete">Delete</button></td>
-                </tr>`);
+                   
+                             </tr>`);
 
                 bindEvent();
             }
@@ -48,7 +51,7 @@ function loadAllCustomers() {
             let parse = JSON.parse(err.responseText);
             alert(parse.message);
         }
-    });
+    })
 }
 
 loadAllCustomers();
