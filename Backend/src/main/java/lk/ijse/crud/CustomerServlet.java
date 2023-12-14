6ab6d35f8e86716e343679f505013ee72a7056c5
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2023-12-14 14:39:41 +0530

    add servlet

---
+++ Backend/src/main/java/lk/ijse/crud/CustomerServlet.java
@@ -0,0 +1,21 @@
+package lk.ijse.crud;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author : savindaJ
+ * @date : 12/14/2023
+ * @since : 0.1.0
+ **/
+@WebServlet(urlPatterns = "/customer")
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("work");
+    }
+}
